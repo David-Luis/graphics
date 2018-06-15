48aa1714bad36c20f89e5d849e0fa17de55ee382
David Luis Alonso <david.luis.87@gmail.com>
2018-06-15 19:34:26 +0200

    set working directory fix

--- 015_Blending/make_project.bat
+++ 015_Blending/make_project.bat
@@ -1,6 +1,10 @@
-if not exist "./Debug" mkdir "./Debug"
-copy "..\Assimp\lib\assimp-vc140-mt.dll" ".\Debug"
-
+@echo off
 if not exist "./project" mkdir "./project"
 cd project
+
+if not exist "./Debug" mkdir "./Debug"
+copy "..\..\Assimp\lib\assimp-vc140-mt.dll" ".\Debug"
+
 cmake ../
+
+cd ..
--- 015_Blending/make_project_and_open.bat
+++ 015_Blending/make_project_and_open.bat
@@ -1,4 +1,11 @@
+if not exist "./project" mkdir "./project"
+cd project
+
 if not exist "./Debug" mkdir "./Debug"
 copy "..\Assimp\lib\assimp-vc140-mt.dll" ".\Debug"
-cmake .
+
+cmake ../
+
 start Application.sln
+
+cd ..
