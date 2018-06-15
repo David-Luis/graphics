d93b3fa19e0b9b1e9bbc1ab808020facc2416b94
David Luis Alonso <david.luis.87@gmail.com>
2018-06-15 19:38:30 +0200

    fix make and open

--- 015_Blending/make_project_and_open.bat
+++ 015_Blending/make_project_and_open.bat
@@ -1,8 +1,9 @@
+@echo off
 if not exist "./project" mkdir "./project"
 cd project
 
 if not exist "./Debug" mkdir "./Debug"
-copy "..\Assimp\lib\assimp-vc140-mt.dll" ".\Debug"
+copy "..\..\Assimp\lib\assimp-vc140-mt.dll" ".\Debug"
 
 cmake ../
 
